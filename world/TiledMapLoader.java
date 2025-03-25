c1289bcb5885f46b6bb0e3d5abd1e3dc181634a7
Betulis01 <Emil.birchxx@hotmail.com>
2025-03-25 21:09:12 +0100

    TileManager update to read JSON-file from Tiled

--- world/TiledMapLoader.java
+++ world/TiledMapLoader.java
@@ -3,6 +3,7 @@ package world;
 import com.google.gson.*;
 
 import main.GamePanel;
+import tile.TileManager;
 import world.decoration.Grass;
 
 import java.awt.image.BufferedImage;
@@ -11,8 +12,7 @@ import java.io.InputStreamReader;
 public class TiledMapLoader {
 
 
-    public static void loadDecorFromTiled(String path, DecorManager decoM,
-                                          BufferedImage[] grassFrames, GamePanel gp) {
+    public static void loadDecorFromTiled(String path, DecorManager decoM, BufferedImage[] grassFrames, GamePanel gp) {
         try {
             // Step 1: Load and parse JSON
             InputStreamReader reader = new InputStreamReader(TiledMapLoader.class.getResourceAsStream(path));
@@ -62,4 +62,57 @@ public class TiledMapLoader {
             e.printStackTrace();
         }
     }
+
+    public static void loadTileLayer(String path, TileManager tileManager) {
+    try {
+        InputStreamReader reader = new InputStreamReader(
+            TiledMapLoader.class.getResourceAsStream(path)
+        );
+        JsonObject root = JsonParser.parseReader(reader).getAsJsonObject();
+
+        JsonArray layers = root.getAsJsonArray("layers");
+
+        for (JsonElement layerElement : layers) {
+            JsonObject layer = layerElement.getAsJsonObject();
+
+            String type = layer.get("type").getAsString();
+            String name = layer.get("name").getAsString();
+
+            if (!"tilelayer".equals(type)) continue;
+            if (!"groundLayer".equalsIgnoreCase(name)) continue;
+
+            int width = layer.get("width").getAsInt();
+            int height = layer.get("height").getAsInt();
+            JsonArray data = layer.getAsJsonArray("data");
+
+            int[][] map = tileManager.mapTileNum;
+
+            //System.out.println("Loading tile layer '" + name + "' (" + width + " x " + height + ")");
+
+            int index = 0;
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    int tileID = data.get(index).getAsInt();
+
+                    if (tileID > 0) {
+                        // Tiled starts tile IDs at 1, tile[] starts at 10
+                        map[col][row] = tileID + 9;
+                    } else {
+                        map[col][row] = 0;
+                    }
+
+                    index++;
+                }
+            }
+
+            break; // we're done after this layer
+        }
+
+    } catch (Exception e) {
+        System.err.println("Error loading tile layer from TMJ:");
+        e.printStackTrace();
+    }
+}
+
+
 }
