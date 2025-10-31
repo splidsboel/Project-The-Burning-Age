5c4e8aba3833db05aed05d49ba52823ee04ff07b
Betulis0 <emil.birchxx@hotmail.com>
2025-10-31 14:51:44 +0100

    stuff

---
+++ app/bin/main/assets/decor/trees/treeWide.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="treeWide" tilewidth="64" tileheight="64" tilecount="4" columns="4">
+ <image source="treeWide.png" width="256" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="3" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="990"/>
+   <frame tileid="2" duration="980"/>
+   <frame tileid="3" duration="970"/>
+   <frame tileid="0" duration="990"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="0" duration="960"/>
+   <frame tileid="1" duration="950"/>
+   <frame tileid="2" duration="940"/>
+   <frame tileid="3" duration="950"/>
+  </animation>
+ </tile>
+</tileset>
--- app/src/main/java/game/entities/actors/npc/Orc.java
+++ app/src/main/java/game/entities/actors/npc/Orc.java
@@ -4,29 +4,137 @@ import engine.core.Game;
 import game.entities.Actor;
 import game.entities.behavior.Collidable;
 import game.entities.behavior.Damageable;
+import game.entities.behavior.Hittable;
+import game.entities.behavior.Moveable;
+import game.states.play.World;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 
-public class Orc extends Actor implements Collidable, Damageable {
+public class Orc extends Actor implements Collidable, Damageable, Moveable, Hittable {
+    private World world;
+    private final Image spriteSheet;
+    private Image[][] animations;
+    private int pixels; 
 
-    public Orc(Game game, double x, double y, double width, double height, double speed) {
-        super(game, x, y, width, height, speed);
-        //TODO Auto-generated constructor stub
+    private boolean up, down, left, right = false;
+    private boolean moving;
+    private double aniTimer;
+    private int aniIndex;
+    private final double aniSpeed = 0.2; // seconds per frame 
+    private final int moveDown = 0, moveUp = 1, moveLeft = 2, moveRight = 3;
+    private int playerAction = moveLeft;
+
+    public Orc(Game game, World world, double x, double y) {
+        super(game, game.getTileSize()*247, game.getTileSize()*250, game.getTileSize()*2, game.getTileSize(), 50);
+        this.world = world;
+        this.spriteSheet = new Image(getClass().getResource("/assets/actors/player/orc.png").toExternalForm());
+        this.pixels = 32;
+
+        loadAnimations();
+        setSolidArea(pixels * 0.42,pixels * 0.85,pixels * 0.15,pixels * 0.08);
+        setHitbox(0.3,0.5);
+    }
+
+    @Override
+    public void update(double delta) {
+        move(delta);
+        updateAnimation(delta);
+        setAnimationDirection();
+    }
+
+    @Override
+    public void render(GraphicsContext g) {
+        Image frame = animations[aniIndex][playerAction];
+        // Draw the player
+        g.drawImage(frame, x, y, width, height);
+
+        g.restore();
+    }
+
+    @Override
+    public void move(double delta) {
+        updateSolidArea();
+        updateHitbox();
+        moving = false;
+
+       
+    }
+
+
+    private void updateAnimation(double delta) {
+        if (moving) {
+                aniTimer += delta;
+                if (aniTimer >= aniSpeed) {
+                    aniTimer = 0;
+                    aniIndex++;
+                    if (aniIndex >= animations.length) {
+                    aniIndex = 1;
+                    }
+                }
+        } else {
+            aniIndex = 0;
+        }
+    }
+
+    private void setAnimationDirection() {
+        if (up)    playerAction = moveUp;
+        if (down)  playerAction = moveDown;
+        if (left)  playerAction = moveLeft;
+        if (right) playerAction = moveRight;
+    }
+
+    @Override
+    public double getBottomY() {
+        if (solidArea != null) {
+            return solidArea.getMaxY(); // already equals y + height
+        }
+        return y + height; // fallback
+    }
+
+
+    private void loadAnimations() {
+        animations = new Image[3][4];
+        for (int i = 0; i < animations.length; i++) {
+            for (int j = 0; j < animations[i].length; j++) {
+                PixelReader reader = spriteSheet.getPixelReader();
+                animations[i][j] = new WritableImage(reader, i * pixels, j * pixels, pixels, pixels);
+            }
+        }
     }
 
     @Override
     public void takeDamage(int amount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'takeDamage'");
+        health -= amount;
+        if (health < 0) health = 0;
     }
 
     @Override
     public boolean isDead() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isDead'");
+        return health <= 0;
+    }
+
+    @Override
+    public Rectangle2D getHitbox() {
+        return hitbox; // or a slightly larger area if you want pickup overlap
+    }
+
+    @Override
+    public Rectangle2D getSolidArea() {
+        return solidArea;
     }
 
     @Override
     public boolean isSolid() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isSolid'");
+        return true; // blocks movement for other entities
+    }
+
+    @Override
+    public void onCollide(Collidable other) {
+        // Example reactions:
+        // if (other instanceof Enemy) takeDamage(1);
+        // if (other instanceof ItemDrop) pickup((ItemDrop) other);
     }
 }
